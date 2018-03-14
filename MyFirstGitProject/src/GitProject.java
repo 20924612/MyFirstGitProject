6c544dc8d40d173bd30aedd34f595dd3ebb5e613
alu20920614k <alu20920614k@ti2-08>
2018-03-14 20:18:11 +0100

    contributor modification

--- MyFirstGitProject/src/GitProject.java
+++ MyFirstGitProject/src/GitProject.java
@@ -3,10 +3,16 @@ public class GitProject {
 
 	private String param1;
 	private String param2;
+	
 	public GitProject(String param1, String param2) {
+		
 		super();
 		this.param1 = param1;
 		this.param2 = param2;
+	}
+	
+	private void showMessage() {
+		System.out.println("olo from AustisticSuicidal");
 	}
 	
 	
